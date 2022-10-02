77aec406ca3a695dbc785fa55b8ac2aeec33ff48
Dattaraj <dattaraj@nimapinfotech.com>
2022-10-02 19:02:13 +0530

    added

--- frontend/extra/_sidebar.php
+++ frontend/extra/_sidebar.php
@@ -12,5 +12,17 @@
          <span class="menu-title">List</span>
          </a>
       </li>
+      <li class="nav-item">
+         <a class="nav-link collapsed" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+           <i class="menu-icon mdi mdi-card-text-outline"></i>
+           <span class="menu-title">Masters</span>
+           <i class="menu-arrow"></i>
+         </a>
+         <div class="collapse" id="form-elements" style="">
+           <ul class="nav flex-column sub-menu">
+             <li class="nav-item"><a class="nav-link" href="../../pages/forms/basic_elements.html">Department</a></li>
+           </ul>
+         </div>
+       </li>
    </ul>
 </nav>
