9722081d503d953ab3726a6c4a0f23f2c8b4ab1a
Dattaraj <dattaraj@nimapinfotech.com>
2022-10-02 16:47:49 +0530

    Department changed

--- frontend/pages/index.php
+++ frontend/pages/index.php
@@ -10,7 +10,7 @@
           <div class="col-lg-4 mx-auto">
             <div class="auth-form-light text-left py-5 px-4 px-sm-5">
               <h4>Login</h4>
-              <form class="pt-3" action="userLogin.php" method="POST">
+              <form class="pt-3" action="userLogin.php" method="POST" onsubmit="return validate();">
                 <div class="form-group">
                   <input type="number" class="form-control form-control-lg" name="userPhone" id="userPhone" placeholder="UserPhone">
                 </div>
@@ -36,48 +36,23 @@
 </body>
 </html>
 <script>
-     function validate()
-     {
-          var phone = document.getElementById("userPhone").value;
-          var password = document.getElementById("userPassword").value;
+  function validate()
+  {
+    var phone = document.getElementById("userPhone").value;
+    var password = document.getElementById("userPassword").value;
 
-          if(phone == "")
-          {
-               alertify.error('Please enter Phone');
-               return false;
-          }
+    if(phone == "")
+    {
+      alertify.error('Please enter Phone');
+      document.getElementById("userPhone").focus();
+      return false;
+    }
 
-          if(password == "")
-          {
-               alertify.error('Please enter Password');
-               return false;
-          }
-
-          var data = {
-               "userPhone":phone,
-               "userPassword":password
-          };
-
-          
-          $.ajax({
-              type: "POST",
-              url: '<?php echo $api_url."auth/userLogin.php";?>',
-              data: JSON.stringify(data),
-              processData: false,
-              contentType: false,
-              success: function (responseDatas) 
-              {
-                var responseData = JSON.parse(responseDatas);
-                if(responseData.flag)
-                {
-                  sessionStorage.setItem("userData",responseDatas);
-                  window.location.href = "basic-table.php";
-                }
-                else
-                {
-                  alertify.error(responseData.message);
-                }
-              }
-          });
-     }
+    if(password == "")
+    {
+      alertify.error('Please enter Password');
+      document.getElementById("userPassword").focus();
+      return false;
+    }
+  }
 </script>
